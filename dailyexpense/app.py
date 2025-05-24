ad5a5ffb4a619c4088319ed0fe4742a69414e867
BinilTomJose1278 <biniltomjose12780@gmail.com>
2025-05-25 00:47:01 +1000

    Daily Expense Refactored

--- dailyexpense/app.py
+++ dailyexpense/app.py
@@ -74,4 +74,5 @@ def sort_expenses():
     return jsonify(sorted_list)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
